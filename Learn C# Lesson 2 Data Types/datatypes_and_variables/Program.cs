d48889fc3a089d8899d84032a04d4fec9606f417
YeshuaNun <145120313+YeshuaNun@users.noreply.github.com>
2024-02-12 17:11:51 +0000

    Initial data types and variables lesson compelte, working with numbers is up next

---
+++ Learn C# Lesson 2 Data Types/datatypes_and_variables/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+/*
+There are two methods for declaring variables:
+Using two lines, eg.
+
+int myAge;
+myAge = 32;
+
+or
+
+In one line, eg.
+
+string countryName = "Netherlands";
+*/
+
+using System;
+
+// namespace Form
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       // Create Variables
+//         string name = "Shadow";
+//         string breed = "Golden Retriever";
+//         int age = 5;
+//         double weight = 65.22;
+//         bool spayed = true;
+
+//       // Print variables to the console
+//         Console.WriteLine(name);
+//         Console.WriteLine(breed);
+//         Console.WriteLine(age);
+//         Console.WriteLine(weight);
+//         Console.WriteLine(spayed);
+//     }
+//   }
+// }
+
+// namespace FavoriteNumber
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       // Ask user for fave number
+//       Console.Write("Enter your favorite number!: ");
+      
+//       // Turn that answer into an int
+//       // int faveNumber = Console.ReadLine();
+//       // int faveNumber = (int)Console.ReadLine();
+//       int faveNumber = Convert.ToInt32(Console.ReadLine());
+
+
+//     }
+//   }
+// }
---
+++ Learn C# Lesson 2 Data Types/learnC#_datatypes_variables.md
@@ -0,0 +1,4 @@
+# Lesson: Data types and variables
+
+###### Image of Data Types within C#
+![alt text](data_types.png)
