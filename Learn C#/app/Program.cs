8d49179b88d7e4bac7cbd45ccbb0b7d60ac45bc8
YeshuaNun <145120313+YeshuaNun@users.noreply.github.com>
2024-02-12 12:24:34 +0000

    First two lessons complete - HelloWorld and Getting Input

---
+++ Learn C#/app/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+// namespace HelloWorld
+// {
+//   class Program
+//   {
+//     static void Main()
+//     {
+//       Console.WriteLine("Joshua Brooks");    
+//      }
+//   }
+// }
+
+// namespace GettingInput
+// {
+//   class Program
+//   {
+//     static void Main()
+//     {
+//       Console.WriteLine("How old are you?");
+//       string input = Console.ReadLine();
+//       Console.WriteLine($"You are {input} years old!");
+//     }
+//   }
+// }
