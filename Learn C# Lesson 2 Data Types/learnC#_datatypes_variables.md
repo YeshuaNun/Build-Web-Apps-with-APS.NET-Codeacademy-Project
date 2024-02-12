d3aad71b63e851de046fc11dcd014f91c154cb4b
YeshuaNun <145120313+YeshuaNun@users.noreply.github.com>
2024-02-12 20:24:37 +0000

    Working with numbers complete, next activity is working with text

--- Learn C# Lesson 2 Data Types/learnC#_datatypes_variables.md
+++ Learn C# Lesson 2 Data Types/learnC#_datatypes_variables.md
@@ -2,3 +2,8 @@
 
 ###### Image of Data Types within C#
 ![alt text](data_types.png)
+
+This lesson focused on the different data types and the first stages of converting between different data types.
+
+# Lesson: Working with numbers
+
---
+++ Learn C# Lesson 2 Data Types/working_with_numbers/Program.cs
@@ -0,0 +1,122 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+// namespace Numbers
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       // Number of pizza shops
+//       int pizzaShops = 4332;
+//       // Number of employees
+//       int totalEmployees = 86928;
+//       // Revenue
+//       decimal revenue = 390819.28m;
+//       // Log the values to the console:
+//       Console.WriteLine(pizzaShops);
+//       Console.WriteLine(totalEmployees);
+//       Console.WriteLine(revenue);
+//    	}
+//   }
+// }
+
+// namespace PlanetCalculations
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {  
+//       // Your Age
+//       int userAge = 34;
+//       // Length of years on Jupiter (in Earth years)
+//       double jupiterYears = 11.86;
+//       // Age on Jupiter
+//       double jupiterAge = userAge/jupiterYears;
+//       // Time to Jupiter
+//       double journeyToJupiter = 6.142466;
+//       // New Age on Earth
+//       double newEarthAge = userAge+journeyToJupiter;
+//       // New Age on Jupiter
+//       double newJupiterAge = newEarthAge/jupiterYears;
+//       // Log calculations to console
+//       Console.WriteLine(userAge);
+//       Console.WriteLine(jupiterAge);
+//       Console.WriteLine(newEarthAge);
+//       Console.WriteLine(newJupiterAge);
+//     }
+//   }
+// }
+
+// namespace MakingProgress
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       // declare steps variable
+//       int steps = 0;
+//       // Two steps forward
+//       steps+=2;
+//       // One step back
+//       steps--;
+//       // Print result to the console
+//       Console.WriteLine(steps);
+//     }
+//   }
+// }
+
+// namespace ClassTeams
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       // Number of students
+//       int students = 18;
+//       // Number of students in a group
+//       int groupSize = 3;      
+//       // Does groupSize go evenly into students?
+//       Console.WriteLine(students%groupSize);
+//     }
+//   }
+// }
+
+// namespace LowestNumber
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       // Starting variables 
+//       int numberOne = 12932;
+//       int numberTwo = -2828472;
+//       // Use built-in methods and save to variable
+//       double numberOneSqrt = Math.Floor(Math.Sqrt(Math.Abs(numberOne)));
+//       // Use built-in methods and save to variable
+//       double numberTwoSqrt = Math.Floor(Math.Sqrt(Math.Abs(numberTwo)));
+//       // Print the lowest number
+//       Console.WriteLine(Math.Min(numberOneSqrt, numberTwoSqrt));
+//     }
+//   }
+// }
+
+// namespace DocumentationHunt
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       double numberOne = 6.5;
+//       double numberTwo = 4;
+//       // Raise numberOne to the numberTwo power
+//       Console.WriteLine(Math.Pow(numberOne, numberTwo));
+//       // Round numberOne up
+//       Console.WriteLine(Math.Ceiling(numberOne));
+//       // Find the larger number between numberOne and numberTwo
+//       Console.WriteLine(Math.Max(numberOne, numberTwo));
+//     }
+//   }
+// }
