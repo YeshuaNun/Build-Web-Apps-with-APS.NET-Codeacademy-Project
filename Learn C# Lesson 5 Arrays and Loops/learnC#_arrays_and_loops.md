19535968af4a6c66ca0b39075455b007e76df9b8
YeshuaNun <145120313+YeshuaNun@users.noreply.github.com>
2024-02-18 13:11:43 +0000

    lesson on loops completed, next fiels will relate to two projects involving arrays and loops

--- Learn C# Lesson 5 Arrays and Loops/learnC#_arrays_and_loops.md
+++ Learn C# Lesson 5 Arrays and Loops/learnC#_arrays_and_loops.md
@@ -5,3 +5,15 @@ This series of lessons focuses on how C# uses arrays to store a collection of va
 # Lesson: Arrays
 
 This lesson focused on how to build, access, and edit values in arrays. It also introduced how to use some built-in methods for arrays.
+
+# Lesson: Loops
+
+Some notes on the different types of loops:
+In general,
+
+- **while** loops are good when you know your stopping condition, but not when you know how many times you want a program to loop or if you have a specific collection to loop through.
+- **do...while** loops are only necessary if you definitely want something to run once, but then stop if a condition is met.
+- **for** loops are best if you want something to run a specific number of times, rather than given a certain condition.
+- **foreach** loops are the best way to loop over an array, or any other collection.
+
+This lesson focused on the use of loops. It explored fours types of loops available when using C#. It also explored how to use break, continue, and return with loops.
--- Learn C# Lesson 5 Arrays and Loops/loops/Program.cs
+++ Learn C# Lesson 5 Arrays and Loops/loops/Program.cs
@@ -1,2 +1,115 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+// namespace WhileLoop
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       int emails = 20;
+//       while (emails>0) {
+//         Console.WriteLine($"You have {emails} emails in your inbox.");
+//         emails--;
+//         Console.WriteLine($"Deleting an email. You have {emails} emails remaining.");
+//       }
+//       Console.WriteLine("INBOX ZERO ACHIEVED!");
+//     }
+//   }
+// }
+
+// namespace DoWhileLoop
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       bool buttonClick = true;
+//       do {
+//         Console.WriteLine("BLARRRRRRRRR");
+//       } while (!buttonClick);
+//       Console.WriteLine("Time for a five minute break.");
+//     }
+//   }
+// }
+
+// namespace ForLoop
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//         for (int i=1; i<17; i++) {
+//             CreateTemplate(i);
+//         }
+//     }    
+//     static void CreateTemplate(int week)
+//     {
+//       Console.WriteLine($"Week {week}");
+//       Console.WriteLine("Announcements: \n \n \n ");
+//       Console.WriteLine("Report Backs: \n \n \n");
+//       Console.WriteLine("Discussion Items: \n \n \n");
+//     }
+//   }
+// }
+
+// namespace ForEachLoop
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       string[] todo = { "respond to email", "make wireframe", "program feature", "fix bugs" };
+//       foreach (string task in todo) {
+//         CreateTodoItem(task);
+//       }
+//     }
+    
+//     static void CreateTodoItem(string item)
+//     {
+//       Console.WriteLine($"[] {item}");
+//     }
+//   }
+// }
+
+// namespace ComparingLoops
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       string[] websites = { "twitter", "facebook", "gmail" };
+//     //   int counter = 0;      
+//     //   while (counter < websites.Length)
+//     //   { 
+//     //     Console.WriteLine(websites[counter]); 
+//     //     counter++; 
+//     //   }
+//       foreach (string website in websites) {
+//         Console.WriteLine(website);
+//       }
+//     }
+//   }
+// }
+
+namespace JumpStatements
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      bool buttonClick = false;
+      int alarmSounds = 0;      
+      do
+      {
+        if (alarmSounds==3) {
+            break;
+        }
+        Console.WriteLine("BLARRRRR");
+        alarmSounds++;        
+      } while(!buttonClick);
+    }
+  }
+}
