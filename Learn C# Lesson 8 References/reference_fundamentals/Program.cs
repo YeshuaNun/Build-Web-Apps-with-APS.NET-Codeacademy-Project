172f9f32b0d0aa96cc16851b12886d794b6ce78c
YeshuaNun <145120313+YeshuaNun@users.noreply.github.com>
2024-02-26 17:38:56 +0000

    lesson - references 8/11 completed

--- Learn C# Lesson 8 References/reference_fundamentals/Program.cs
+++ Learn C# Lesson 8 References/reference_fundamentals/Program.cs
@@ -45,20 +45,52 @@ using System;
 //   }
 // }
 
+// namespace LearnReferences
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       Dissertation diss = new Dissertation();
+// 			IFlippable fdiss = diss;
+//       Book bdiss = diss;
+// 			fdiss.CurrentPage = 42;
+//     //   bdiss.CurrentPage = 43;
+//     //   fdiss.Stringify();
+//       bdiss.Stringify();
+//       Console.WriteLine(fdiss == bdiss);
+//     }
+//   }
+// }
+
+// namespace LearnReferences
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       Dissertation diss1 = new Dissertation(32, "Anna Knowles-Smith", "Refugees and Theatre");
+// 	  Dissertation diss2 = new Dissertation(19, "Lajos Kossuth", "Shiny Happy People");
+// 	  Diary dy1 = new Diary(48, "Anne Frank", "The Diary of a Young Girl");
+// 	  Diary dy2 = new Diary(23, "Lili Elbe", "Man into Woman");
+//       Book[] books = {diss1, diss2, dy1, dy2};
+//       foreach (Book book in books) {
+//         Console.WriteLine(book.Title);
+//       }      
+//     }
+//   }
+// }
+
 namespace LearnReferences
 {
   class Program
   {
     static void Main(string[] args)
     {
-      Dissertation diss = new Dissertation();
-			IFlippable fdiss = diss;
-      Book bdiss = diss;
-			fdiss.CurrentPage = 42;
-    //   bdiss.CurrentPage = 43;
-    //   fdiss.Stringify();
-      bdiss.Stringify();
-      Console.WriteLine(fdiss == bdiss);
+      Book b1 = new Book();
+      Book b2 = new Diary();
+      Console.WriteLine(b1.Stringify());
+      Console.WriteLine(b2.Stringify());      
     }
   }
 }
