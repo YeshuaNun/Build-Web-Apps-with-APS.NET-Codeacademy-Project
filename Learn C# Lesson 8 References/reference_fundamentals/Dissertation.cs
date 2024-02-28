93681390a549ca30ca7e5b03edcd8eca88123730
YeshuaNun <145120313+YeshuaNun@users.noreply.github.com>
2024-02-28 18:54:43 +0000

    lesson reference fundamentals completed, next lesson looks at further aspects of the object class

--- Learn C# Lesson 8 References/reference_fundamentals/Dissertation.cs
+++ Learn C# Lesson 8 References/reference_fundamentals/Dissertation.cs
@@ -1,3 +1,32 @@
+// namespace LearnReferences
+// {
+//   class Dissertation : Book, IFlippable
+//   {
+//     public int CurrentPage
+//     { get; set; }
+    
+//     public Dissertation(int page = 0) : base()
+//     {
+//       CurrentPage = page;
+//     }
+
+//     public void Flip()
+//     {
+//       CurrentPage++;
+//     }
+
+//     public string Define()
+//     {
+//       return "dissertation - a long essay on a particular subject";
+//     }
+
+//     public override string Stringify() 
+//     {
+//       return "This is a Dissertation object!";
+//     }
+//   }
+// }
+
 namespace LearnReferences
 {
   class Dissertation : Book, IFlippable
@@ -6,6 +35,11 @@ namespace LearnReferences
     { get; set; }
     
     public Dissertation(int page = 0) : base()
+    {
+      CurrentPage = page;
+    }
+    
+    public Dissertation(int page, string author, string title) : base(author, title)
     {
       CurrentPage = page;
     }
--- Learn C# Lesson 8 References/reference_fundamentals/Program.cs
+++ Learn C# Lesson 8 References/reference_fundamentals/Program.cs
@@ -81,16 +81,115 @@ using System;
 //   }
 // }
 
+// namespace LearnReferences
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       Book b1 = new Book();
+//       Book b2 = new Diary();
+//       Console.WriteLine(b1.Stringify());
+//       Console.WriteLine(b2.Stringify());      
+//     }
+//   }
+// }
+
+// namespace LearnReferences
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+// 			Dissertation diss = new Dissertation();
+//       Diary dy = new Diary();
+      
+//       Book bdiss = diss;
+//       Book bdy = dy;
+//     }
+//   }
+// }
+
+// namespace LearnReferences
+// {
+//   class Program
+//   {
+//     static void Main(string[] args)
+//     {
+//       Book bk = null;
+//       Console.WriteLine(bk);
+//       Console.WriteLine(bk==null);
+//     }
+//   }
+// }
+
 namespace LearnReferences
 {
   class Program
   {
     static void Main(string[] args)
     {
-      Book b1 = new Book();
-      Book b2 = new Diary();
-      Console.WriteLine(b1.Stringify());
-      Console.WriteLine(b2.Stringify());      
+      // Two references to the same object
+      Dissertation diss1 = new Dissertation();
+      Dissertation diss2 = diss1;
+      diss1.CurrentPage = 31;
+      diss1.Flip();
+      Console.WriteLine(diss1.CurrentPage);
+      Console.WriteLine(diss2.CurrentPage);
+      // Both print 32
+
+      // Referential equality when comparing reference types
+      Dissertation d1 = new Dissertation(50);
+      Dissertation d2 = new Dissertation(50);
+      Console.WriteLine(d1 == d2);
+      // Prints false
+
+      // Value equality when comparing value types
+      int int1 = 6;
+      int int2 = 6;
+      Console.WriteLine(int1 == int2);
+      // Prints true
+
+      // Reference to object with implemented interface
+      Dissertation diss = new Dissertation(50);
+      IFlippable f = diss;
+      diss.Flip();
+      f.Flip();
+      Console.WriteLine(diss.Define());
+      // This causes an error!
+      // Console.WriteLine(f.Define());
+
+      // Reference to object with inherited superclass
+      Dissertation diss3 = new Dissertation(19, "Lajos Kossuth", "Shiny Happy People");
+      Book bdiss3 = diss3;
+      Console.WriteLine(diss3.Author);
+      Console.WriteLine(bdiss3.Author);
+      Console.WriteLine(diss3.Define());
+      // This causes an error!
+      // bdiss3.Define();
+
+      // Arrays of references
+      IFlippable[] flippers = new IFlippable[] {new Dissertation(), new Diary()};
+      foreach (IFlippable flipper in flippers)
+      {
+        flipper.Flip();
+      }
+      
+      // Same reference type but different behavior (using virtual/override)
+      Book bdiss = new Dissertation();
+      Book bk = new Book();
+      Console.WriteLine(bdiss.Stringify());
+      Console.WriteLine(bk.Stringify());
+      // Output:
+      // "This is a Dissertation object!"
+      // "This is a Book object!"
+      
+      // Null reference
+      Diary dy = null;
+      // Unassigned reference
+      Diary dy2;
+      // Unassigned references in array
+      Diary[] dys = new Diary[5];
     }
   }
 }
