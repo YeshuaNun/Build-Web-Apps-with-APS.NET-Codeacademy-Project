5505973929c7e8bfedb6c9803fe31715e92322d8
YeshuaNun <145120313+YeshuaNun@users.noreply.github.com>
2024-02-29 17:28:49 +0000

    project - rover control center started, part 5/10 completed

--- Learn C# Lesson 8 References/project-rover_control_center/Program.cs
+++ Learn C# Lesson 8 References/project-rover_control_center/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace RoverControlCenter
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      MoonRover lunokhod = new MoonRover("Lunokhod 1", 1970);
+      MoonRover apollo = new MoonRover("Apollo 15", 1971);
+      MarsRover sojourner = new MarsRover("Sojourner", 1997);
+      Satellite sputnik = new Satellite("Sputnik", 1957);
+
+      Rover[] rovers = {lunokhod, apollo, sojourner};
+      DirectAll(rovers);
+
+      Object[] probes = {lunokhod, apollo, sojourner, sputnik};
+      foreach (Object probe in probes) {
+        Console.WriteLine($"Tracking a {probe.GetType()}.");
+      }
+    }
+
+    public static void DirectAll(Rover[] rovers) {
+        foreach (Rover rover in rovers) {
+            Console.WriteLine(rover.GetInfo());
+            Console.WriteLine(rover.Explore());
+            Console.WriteLine(rover.Collect());
+        }
+    } 
+  }
+}
--- Learn C# Lesson 8 References/project-rover_control_center/obj/Debug/net8.0/project-rover_control_center.AssemblyInfo.cs
+++ Learn C# Lesson 8 References/project-rover_control_center/obj/Debug/net8.0/project-rover_control_center.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project-rover_control_center")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cd7d97d8b928022d3954830df1ba94a1f51569e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba87e8a2adbe8cf2e7a511b42f8e8589ec3484bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("project-rover_control_center")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project-rover_control_center")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
